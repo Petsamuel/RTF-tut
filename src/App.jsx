a2933d8e6ac6009de2cbc0b2d4853d325d39a796
petsamuel <petsamuel4@gmail.com>
2024-03-17 20:23:21 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable react/no-unknown-property */
-
 import { Fragment } from 'react'
 import { Canvas } from '@react-three/fiber'
 
@@ -10,10 +8,10 @@ function App() {
       <section className='flex  items-center mt-[10em]'>
         <Canvas >
           <ambientLight intensity={0.1} />
-          <directionalLight color="red" position={[0, 1, 5]} />
+          <directionalLight color="red" position={[2, 2, 5]} />
           <mesh>
             <boxGeometry args={[4, 4, 4]} />
-            <meshStandardMaterial  />
+            <meshStandardMaterial />
           </mesh>
         </Canvas>
       </section>
